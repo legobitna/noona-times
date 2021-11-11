2bc79b1461540ee555c84cefb7d536676050c907
Bitna Kim <tallshe108@hanmail.net>
2021-11-11 15:59:39 +0900

    change api

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const API_KEY = "XGtguBOOaRPYsGvo9lKKmMgUlmHxcMLX";
 const news_api = "b1fe516cb2ff4032b010ec5773f3a973";
+
+//https://app.newscatcherapi.com/dashboard/
+
 let articles = [];
 const getapi = async () => {
-  let url = `https://newsapi.org/v2/everything?q=tesla&from=2021-10-11&sortBy=publishedAt&apiKey=${news_api}`;
-  let response = await fetch(url);
+  let url = `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=10`;
+  let h = new Headers();
+  h.append("x-api-key", `HAb-VwD8jA1lFdszL-lwITEkRTIlYrTwPo-eSBa-IQk`);
+  //   let url = `https://newsapi.org/v2/everything?q=tesla&from=2021-10-11&sortBy=publishedAt&apiKey=${news_api}`;
+  let response = await fetch(url, { method: "GET", headers: h });
   let data = await response.json();
   articles = data.articles;
-  console.log(articles);
+  console.log(data);
   render();
 };
 
